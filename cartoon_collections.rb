cc5e0150e7129ea16cc488534dc9d8cf73f3e42a
Gus Bikos <gusbikos@gmail.com>
2021-01-18 22:52:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
 
-  array.each_with_index(1) do |element, index|
+  array.each_with_index(1) do |element, index = 1|
     puts "#{index}. #{element}"
   end
 end
